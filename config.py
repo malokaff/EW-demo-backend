6496accb79aadf19a2165e97077e4436f8e22c73
malokaff <malokaff@free.fr>
2023-06-08 11:59:55 +0000

    add tcp ratio

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 #parameter for mysql-publish.py & mqtt-publish.py
 ip_mysql = '10.29.21.15'
 ip_mqttbroker = '10.29.21.15'
-user_mysql = 'root'
-pwd_mysql='xxx'
+user_mysql = 'mqtt'
+pwd_mysql='mqtt'
 usr_mqtt = 'mqtt'
-pwd_mqtt = 'xxx'
+pwd_mqtt = 'mqtt'
 ratio_session = 10
